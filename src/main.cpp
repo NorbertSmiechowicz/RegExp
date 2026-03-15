66f952a0aff23a3326f32cc7036383551117b357
norbert smiechowicz <norbert.smiechowicz@gmail.com>
2026-03-15 12:13:57 +0100

    Makra do generowania wzorców i enumów: one source of truth

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,62 @@
 
 #include "fwdstridx.h"
 #include <memory>
+#include <vector>
 
 #define RC( T, X) reinterpret_cast< T>( X)
 #define SC( T, X) static_cast< T>( X)
 
-enum class ArgIds : long
+template< typename ArgId, typename ArgType>
+struct CommandLineArgumentTemplate
 {
-    AB = 1,
-    AC,
-    AD,
-    C
+    ArgId                 Id;
+    ArgType               Type;
+    const std::string     ShortName;
+    const std::string     LongName;
+    const std::string     HelpInfo;
 };
 
+enum class CliArgType
+{
+    Flag,
+    Sequence
+};
+
+#define CLI_ARG_DEFS( WRAPPER, ID, TYPE, SNAME, LNAME, HELP)\
+    WRAPPER( ID( Help)          TYPE( CliArgType::Flag)             SNAME( "h")     LNAME( "help")      HELP( "Displays this message.")) \
+    WRAPPER( ID( File)          TYPE( CliArgType::Sequence)         SNAME( "f")     LNAME( "files")     HELP( "Files to perform search on."))
+
+
+#define VANISH( X)
+#define IDENTITY( X) X
+#define APPEND_COMMA( X) X,
+
+enum class CliArgId
+{
+    CLI_ARG_DEFS( IDENTITY, APPEND_COMMA, VANISH, VANISH, VANISH, VANISH)
+};
+
+using CliArgTemplate = CommandLineArgumentTemplate< CliArgId, CliArgType>;
+
+
+#define DECL_CLI_ARG_TEMPLATE( X) CliArgTemplate{ X},
+#define PASS_ENUM( X) CliArgId::X
+#define PASS_TEXT( X) , X
+
+static std::vector< CliArgTemplate> const CommandLineArgTemplates
+{
+    CLI_ARG_DEFS( DECL_CLI_ARG_TEMPLATE, PASS_ENUM, PASS_TEXT, PASS_TEXT, PASS_TEXT, PASS_TEXT)
+};
 
 int main( int /*argc*/, char* /*argv*/[])
 {
     ForwardStringIndexFactory idxFactory;
 
-#define IDX_ADD( factory, name) factory.add( "" #name "", RC( void *, ArgIds::name))
-
-    IDX_ADD( idxFactory, AB);
-    IDX_ADD( idxFactory, AB);
-    IDX_ADD( idxFactory, AC);
-    IDX_ADD( idxFactory, AD);
-    IDX_ADD( idxFactory, C);
-
-#undef IDX_ADD
-
     std::unique_ptr< ForwardStringIndex> fsi{ idxFactory.emit()};
 
-
-    printf( "%ld\n", RC( long, fsi->find( "D")));
-    printf( "%ld\n", RC( long, fsi->find( "AB")));
-    printf( "%ld\n", RC( long, fsi->find( "AC")));
+    printf( "%ld\n", RC( long, fsi->find( "h", 1)));
+    printf( "%ld\n", RC( long, fsi->find( "AB", 2)));
+    printf( "%ld\n", RC( long, fsi->find( "AC", 2)));
 
 
     return 0;
